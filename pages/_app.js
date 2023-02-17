4a57c20815f98219247ec69a91b85470a4e37cad
Kfish <kfish@kfishnchips.com>
2023-02-17 15:22:56 -0300

    fix scroll

--- pages/_app.js
+++ pages/_app.js
@@ -9,12 +9,42 @@ function MyApp({ Component, pageProps }) {
     <>
       <Web3OnboardProvider web3Onboard={web3Onboard}>
         <Head>
-          <title>Invictus Order</title>
-          <meta content="width=device-width, initial-scale=1" name="viewport" />
+          <title>Invictus Order by IH</title>
+          <meta name="description" content="Invictus Order by Iron Hills" />
+          <meta charset="UTF-8" />
           <meta
-            name="description"
-            content="Invictus Order, created by Iron Hills, is the culmination of over a year's worth of trying to push the boundaries of NFT art and technology. Knights art is inspired by the online game Spiral Knights. By owning a Knight, you have enlisted as part of the Knights Task Force, an elite squad established to solve the mystery of The Altar."
+            name="viewport"
+            content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1"
           />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta
+            name="twitter:image"
+            content="https://invictusorder.com/og.png"
+          />
+          <meta name="twitter:title" content="Invictus Order by Iron Hills" />
+          <meta name="twitter:site" content="@invictusorder" />
+          <meta name="twitter:creator" content="@invictusorder" />
+          <meta
+            name="twitter:description"
+            content="The Sector Task for has begun to assemble to discover the mystery of The Altar. Only Prismatians with a reflection will be enlisted into the Sector Task Force. The Invictus Order Application is a soulbound token that acts as your on-chain proof of submission and it will dynamically change if your application is accepted. To submit an application to join the Invictus Order, go to https://www.invictusorder.com/. For more information about the allowlist and SBT, go to https://mirror.xyz/ironhills.eth/tI53g0xxPiaYu_3Vp8oZIqPCZCfpsUnq4lpvo_YId3U."
+          />
+          <meta property="og:title" content="Invictus Order by Iron Hills" />
+          <meta
+            property="og:description"
+            content="The Sector Task for has begun to assemble to discover the mystery of The Altar. Only Prismatians with a reflection will be enlisted into the Sector Task Force. The Invictus Order Application is a soulbound token that acts as your on-chain proof of submission and it will dynamically change if your application is accepted. To submit an application to join the Invictus Order, go to https://www.invictusorder.com/. For more information about the allowlist and SBT, go to https://mirror.xyz/ironhills.eth/tI53g0xxPiaYu_3Vp8oZIqPCZCfpsUnq4lpvo_YId3U."
+          />
+          <meta
+            property="og:site_name"
+            content="Invictus Order by Iron Hills"
+          />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://invictusorder.com" />
+          <meta
+            property="og:image"
+            content="https://invictusorder.com/og.png"
+          />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
           <link rel="icon" href="/favicon.png" />
         </Head>
 
--- pages/index.js
+++ pages/index.js
@@ -6,40 +6,9 @@ import ProcessComponent from "../components/processComponent";
 import TeamComponent from "../components/teamComponent";
 import FaqComponent from "../components/faqComponent";
 import ModalQuestionsComponent from "../components/modalQuestionsComponent";
-import Head from "next/head";
 export default function Home() {
   return (
-    <div className="bg-background only:min-h-screen overflow-x-hidden z-0 w-screen">
-      <Head>
-        <title>Invictus Order by IH</title>
-        <meta name="description" content="Invictus Order by Iron Hills" />
-        <meta charset="UTF-8" />
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1"
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content="https://invictusorder.com/og.png" />
-        <meta name="twitter:title" content="Invictus Order by Iron Hills" />
-        <meta name="twitter:site" content="@invictusorder" />
-        <meta name="twitter:creator" content="@invictusorder" />
-        <meta
-          name="twitter:description"
-          content="The Sector Task for has begun to assemble to discover the mystery of The Altar. Only Prismatians with a reflection will be enlisted into the Sector Task Force. The Invictus Order Application is a soulbound token that acts as your on-chain proof of submission and it will dynamically change if your application is accepted. To submit an application to join the Invictus Order, go to https://www.invictusorder.com/. For more information about the allowlist and SBT, go to https://mirror.xyz/ironhills.eth/tI53g0xxPiaYu_3Vp8oZIqPCZCfpsUnq4lpvo_YId3U."
-        />
-        <meta property="og:title" content="Invictus Order by Iron Hills" />
-        <meta
-          property="og:description"
-          content="The Sector Task for has begun to assemble to discover the mystery of The Altar. Only Prismatians with a reflection will be enlisted into the Sector Task Force. The Invictus Order Application is a soulbound token that acts as your on-chain proof of submission and it will dynamically change if your application is accepted. To submit an application to join the Invictus Order, go to https://www.invictusorder.com/. For more information about the allowlist and SBT, go to https://mirror.xyz/ironhills.eth/tI53g0xxPiaYu_3Vp8oZIqPCZCfpsUnq4lpvo_YId3U."
-        />
-        <meta property="og:site_name" content="Invictus Order by Iron Hills" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://invictusorder.com" />
-        <meta property="og:image" content="https://invictusorder.com/og.png" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <link rel="icon" href="/favicon.png" />
-      </Head>
+    <div className="bg-background only:min-h-screen overflow-x-hidden z-0">
       <HeroComponent />
       <AboutComponent />
       <ProcessComponent />
